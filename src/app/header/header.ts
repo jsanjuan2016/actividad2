d09b30edb7d74b93d8ae0ad0e4ce8cc5516e73f7
Javier <jsartegain@gmail.com>
2025-10-13 09:01:12 +0200

    WIP

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
+  isCategoriasOpen: boolean = false;
+  toggleCategoriasMenu(): void {
+    this.isCategoriasOpen = !this.isCategoriasOpen;
+  }
 }
