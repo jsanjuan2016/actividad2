097d403632eeb3be7f46d77674e8af8a24aa64bf
jsanjuan at etxea <jsartegain@gmail.com>
2025-10-13 19:10:06 +0200

    WIP: Añadir servicio para recoger  la infor de la foto

--- src/app/galeria/galeria.ts
+++ src/app/galeria/galeria.ts
@@ -1,12 +1,14 @@
 import { RouterLink } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { routes } from '../app.routes';
+import { PicsumService } from '../services/picsum.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface Photo {
   id: number;
   url: string;
-  author: string;
+  author: Observable<string>; //author: string;
 }
 
 @Component({
@@ -19,21 +21,44 @@ interface Photo {
 // }
 export class Galeria implements OnInit {
 
+  private imageService = inject(PicsumService);
+
   //Images indexes from picsum.photos desde 0 a 1084
+  imgIndex: number[] = [1018, 1025, 1039, 1060, 1084, 1074];
+  //Image dimensions
   widthImg: number = 300;
   heightImg: number = 200;
   
-  photos: Photo[] = [
-    { url: `https://picsum.photos/id/1018/${this.widthImg}/${this.heightImg}`, author: 'Montañas y cielo', id: 1018 },
-    { url: `https://picsum.photos/id/1025/${this.widthImg}/${this.heightImg}`, author: 'Cachorro en la hierba', id: 1025 },
-    { url: `https://picsum.photos/id/1039/${this.widthImg}/${this.heightImg}`, author: 'Puente y río', id: 1039 },
-    { url: `https://picsum.photos/id/1060/${this.widthImg}/${this.heightImg}`, author: 'Tetería', id: 1060 },
-    { url: `https://picsum.photos/id/1084/${this.widthImg}/${this.heightImg}`, author: 'Leones marinos', id: 1084 },
-    { url: `https://picsum.photos/id/1074/${this.widthImg}/${this.heightImg}`, author: 'https://picsum.photos/id/1074/info', id: 1074 }
-  ];
+  photos: Photo[] = [];
+  // photos: Photo[] = [
+  //   { url: `https://picsum.photos/id/1018/${this.widthImg}/${this.heightImg}`, author: 'Montañas y cielo', id: 1018 },
+  //   { url: `https://picsum.photos/id/1025/${this.widthImg}/${this.heightImg}`, author: 'Cachorro en la hierba', id: 1025 },
+  //   { url: `https://picsum.photos/id/1039/${this.widthImg}/${this.heightImg}`, author: 'Puente y río', id: 1039 },
+  //   { url: `https://picsum.photos/id/1060/${this.widthImg}/${this.heightImg}`, author: 'Tetería', id: 1060 },
+  //   { url: `https://picsum.photos/id/1084/${this.widthImg}/${this.heightImg}`, author: 'Leones marinos', id: 1084 },
+  //   { url: `https://picsum.photos/id/1074/${this.widthImg}/${this.heightImg}`, author: 'Leona', id: 1074 }
+  // ];
+  
+  // for (let id:number of this.imgIndex) {
+  //   this.photos.push({
+  //     id: id,
+  //     url: `https://picsum.photos/id/${id}/${this.widthImg}/${this.heightImg}`,
+  //     author: `https://picsum.photos/id/${id}/info`
+  //   });
+  // }
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.photos = this.imgIndex.map(id => ({
+      id: id,
+      url: `https://picsum.photos/id/${id}/${this.widthImg}/${this.heightImg}`,
+      //author: `https://picsum.photos/id/${id}/info`
+      author: this.imageService.getImageInfo(`https://picsum.photos/id/${id}/info`).pipe(
+        map(info => info.author) 
+      )
+    }));
+  
   }
 }
