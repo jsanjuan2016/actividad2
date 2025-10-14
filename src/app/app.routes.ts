a1baf3e3c5ff4014f9c8413759dbb722687047d3
jsanjuan at etxea <jsartegain@gmail.com>
2025-10-14 18:57:37 +0200

    WIP: Añadir placeholder de imagenes no existentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,9 @@ import { GaleriaAleatoria } from './galeria-aleatoria/galeria-aleatoria';
 import { GaleriaPaginada } from './galeria-paginada/galeria-paginada';
 
 export const routes: Routes = [
-  //{ path: '', component: Galeria, outlet:'rate', pathMatch: 'full', title: 'Galeria - actividad2', data: { app: App } }, //PRUEBAS
-  { path: '', component: GaleriaAleatoria, title: 'Galeria - actividad2', data: { app: App } }, //PRUEBAS
+  { path: '', component: GaleriaAleatoria, title: 'Galeria - actividad2', data: { app: App } },
   { path: 'galeria', component: Galeria },
   { path: 'galeria-paginada', component: GaleriaPaginada },
-  //{ path: 'galeria-aleatoria', loadComponent: () => import('./galeria-aleatoria/galeria-aleatoria').then(m => m.GaleriaAleatoria) },
   { path: 'galeria-aleatoria', component: GaleriaAleatoria },
   { path: 'photo/:id/:title', component: Photo },
   { path: '**', component: NotFound }
