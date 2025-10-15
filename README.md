27f36772225c7a210b0d75e9f527167f50f95b4c
jsanjuan at etxea <jsartegain@gmail.com>
2025-10-15 08:59:38 +0200

    Auto-generated commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Actividad2
-
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.5.
--- src/app/models/picsum.info.ts
+++
@@ -1,8 +0,0 @@
-export interface PicsumInfo {
-    id: string;
-    author: string;
-    width: number;
-    height: number;
-    url: string;
-    download_url: string;
-}
